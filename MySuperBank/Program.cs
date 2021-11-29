bc21437852332852b03dfeb6fabc160ff53ca9cc
TCC-RPL-07 <TCC-RPL-07@DESKTOP-AJJTMJG>
2021-11-29 07:04:51 +0530

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Danushka",10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+            account.MakeWithdrawal(120, DateTime.Now,"Hammock");
+            account.MakeWithdrawal(720, DateTime.Now, "XBOX");
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+
+
+}
